d0f11f04f8e93457296b61a523084aa5b937a226
Marvin Drees <marvin.drees@9elements.com>
2023-01-16 11:53:29 +0100

    feat: upgrade old dagger to new dagger NodeSDK
    This remove the legacy dagger 0.2 engine code and moves the project
    over to the new NodeSDK. This means we don't have to download the dagger
    tooling upfront but can invoke the engine directly from yarn via the
    new ci/* files.
    
    Signed-off-by: Marvin Drees <marvin.drees@9elements.com>

---
+++ ci/build.ts
@@ -0,0 +1,27 @@
+import Client, { connect } from '@dagger.io/dagger';
+import type { Container, Directory } from '@dagger.io/dagger/dist/api/client.gen';
+
+// initialize Dagger client
+connect(
+  async (client: Client) => {
+    // get reference to the local project
+    const source: Directory = client
+      .host()
+      .directory('.', { exclude: ['node_modules/', 'build/', '.svelte-kit'] });
+
+    // get Node image
+    const node: Container = client.container().from('node:18.12-alpine3.17');
+
+    // prepare runner
+    const runner: Container = client
+      .container({ id: node })
+      .withMountedDirectory('/src', source)
+      .withWorkdir('/src')
+      .withExec(['yarn', 'install'])
+      .withExec(['yarn', 'run', 'build']);
+
+    // start runner
+    await runner.directory('build/').export('./build');
+  },
+  { LogOutput: process.stdout }
+);
---
+++ ci/test.ts
@@ -0,0 +1,29 @@
+import Client, { connect } from '@dagger.io/dagger';
+import type { Container, Directory } from '@dagger.io/dagger/dist/api/client.gen';
+
+// initialize Dagger client
+connect(
+  async (client: Client) => {
+    // get reference to the local project
+    const source: Directory = client
+      .host()
+      .directory('.', { exclude: ['node_modules/', 'build/', '.svelte-kit'] });
+
+    // get Playwright image
+    const playwright: Container = client
+      .container()
+      .from('mcr.microsoft.com/playwright:v1.29.2-focal');
+
+    // prepare runner
+    const runner: Container = client
+      .container({ id: playwright })
+      .withMountedDirectory('/src', source)
+      .withWorkdir('/src')
+      .withExec(['yarn', 'install'])
+      .withExec(['yarn', 'run', 'test']);
+
+    // start runner
+    await runner.directory('coverage/').export('./coverage');
+  },
+  { LogOutput: process.stdout }
+);
